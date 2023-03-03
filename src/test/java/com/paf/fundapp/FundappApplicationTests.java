3ecd80f44850da000511ae1b7802279e402c967f
DylanTHK <dylantanhk@gmail.com>
2023-03-03 09:08:00 +0800

    first commit

---
+++ src/test/java/com/paf/fundapp/FundappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paf.fundapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
